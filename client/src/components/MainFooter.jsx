3b9373a68045a94c212178f13ef2ad5fae3994cd
Ramiro Ocampo Rodriguez <101475348+ROR2022@users.noreply.github.com>
2024-03-31 14:47:59 -0600

    Addign link to Code

--- client/src/components/MainFooter.jsx
+++ client/src/components/MainFooter.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaCode } from 'react-icons/fa'
 
 const MainFooter = () => {
   return (
     <div>
         <footer className="bg-body-tertiary p-3 d-flex justify-content-around aling-items-center">
             <p className=''>MyLoginApp &copy; 2024</p>
+            <a href="https://github.com/ROR2022/loginApp">
+              <span className='btn btn-light'>
+                <FaCode/>
+              </span>
+            </a>
             <Link to='/terms' className='btn btn-link'>Terms & Conditions</Link>
             
         </footer>
--- client/src/components/MainNavbar.jsx
+++ client/src/components/MainNavbar.jsx
@@ -3,6 +3,8 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo1 from '../assets/fog1.jpg';
+import { FaCode } from "react-icons/fa";
+
 
 
 const MainNavbar = () => {
@@ -16,6 +18,9 @@ const MainNavbar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
+          <Nav.Link href="https://github.com/ROR2022/loginApp">
+              <span className=''><FaCode/></span>
+            </Nav.Link>
             <Nav.Link href="#/languages">Languages</Nav.Link>
             <Nav.Link href="#/support">Support</Nav.Link>
             <Nav.Link href="#/contact">Contac Us</Nav.Link>
